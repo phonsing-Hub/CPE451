7f9cbad900408cca20b603f5248ef0f86ca2b19f
Woranit5419 <beem13580@gmail.com>
2024-04-22 13:52:48 +0700

    Beembill

---
+++ src/Components/Bill Payment/UploadSlip.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const UploadSlip = () => {
+  return (
+    <View>
+      <Text>UploadSlip</Text>
+    </View>
+  )
+}
+
+export default UploadSlip
+
+const styles = StyleSheet.create({})
