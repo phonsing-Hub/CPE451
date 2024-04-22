28888970c64a122048a258c01f4ef13211aa0de2
p.phonsing_ ✅ <158476440+phonsing-Hub@users.noreply.github.com>
2024-04-23 00:01:08 +0700

    style

--- src/Components/Contract.jsx
+++ src/Components/Contract.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, Button ,TouchableOpacity} from 'react-native';
+import { View, Text, StyleSheet, SafeAreaView ,TouchableOpacity} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import AntDesign from 'react-native-vector-icons/AntDesign'
 
@@ -38,6 +38,7 @@ const Contract = () => {
   };
 
   return (
+    <SafeAreaView style={{backgroundColor: '#e7eaf6', flex:1}}>
     <View style={styles.container}>
       <View style={styles.headerBackground}>
         <Text style={styles.headerText}>ข้อมูลการเช่าห้องพัก</Text>
@@ -45,30 +46,31 @@ const Contract = () => {
       <View style={[styles.section, styles.backButton]}>
         <View style={styles.circularButton}>
         <TouchableOpacity onPress={()=> navigation.goBack()}>
-          <AntDesign name='left' size={36} color={'#CA9CAC'}/>
+          <AntDesign name='left' size={36} color={'#a2a8d3'}/>
           </TouchableOpacity>     
         </View>
       </View>
 
-     <View style={{backgroundColor:'#CA9CAC',borderRadius: 15,width:'100%',height:200,justifyContent:'center'}}> 
+     <View style={{backgroundColor:'#a2a8d3',borderRadius: 15,width:'100%',height:200,justifyContent:'center'}}> 
     <View style={{flexDirection: 'row',justifyContent:'space-around',marginBottom:10}}>
-      <TouchableOpacity onPress={handleRoom} style ={{backgroundColor:'#f2eddd',width:100,height:100, justifyContent:'center',alignItems:'center',borderRadius: 15,}}>
+      <TouchableOpacity onPress={handleRoom} style ={{backgroundColor:'#fefefe',width:100,height:100, justifyContent:'center',alignItems:'center',borderRadius: 15,}}>
       <AntDesign name='home' size={36} color={'#a2a8d3'} />
-      <Text style={{fontSize:16,color:'#a2a8d3'}}>ข้อมูลห้องพัก</Text>
+      <Text style={{fontSize:16,color:'#000000'}}>ข้อมูลห้องพัก</Text>
       </TouchableOpacity>
-      <TouchableOpacity onPress={handleReturn} style ={{backgroundColor:'#f2eddd',width:100,height:100, justifyContent:'center',alignItems:'center',borderRadius: 15,}}>
+      <TouchableOpacity onPress={handleReturn} style ={{backgroundColor:'#fefefe',width:100,height:100, justifyContent:'center',alignItems:'center',borderRadius: 15,}}>
       <AntDesign name='contacts' size={36} color={'#a2a8d3'} />
-      <Text style={{fontSize:16,color:'#a2a8d3'}}>ข้อมูลการเช่า</Text>
+      <Text style={{fontSize:16,color:'#000000'}}>ข้อมูลการเช่า</Text>
       </TouchableOpacity>
-      <TouchableOpacity onPress={handleHire} style ={{backgroundColor:'#f2eddd',width:100,height:100, justifyContent:'center',alignItems:'center',borderRadius: 15,}}>
+      <TouchableOpacity onPress={handleHire} style ={{backgroundColor:'#fefefe',width:100,height:100, justifyContent:'center',alignItems:'center',borderRadius: 15,}}>
       <AntDesign name='export2' size={36} color={'#a2a8d3'} />
-      <Text style={{fontSize:16,color:'#a2a8d3'}}>ข้อมูลคืนห้อง</Text>
+      <Text style={{fontSize:16,color:'#000000'}}>ข้อมูลคืนห้อง</Text>
       </TouchableOpacity>
       
 
       </View>
       </View>
     </View>
+    </SafeAreaView>
   );
 };
 
@@ -76,10 +78,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
-    backgroundColor: '#f2eddd', // Background color
+    backgroundColor: '#e7eaf6', // Background color
   },
   headerBackground: {
-    backgroundColor: '#a2a8d3',
+    backgroundColor: '#fefefe',
     borderRadius: 8,
     padding: 10,
     marginBottom: 20,
@@ -91,7 +93,7 @@ const styles = StyleSheet.create({
     fontSize: 28,
     fontWeight: 'bold',
     textAlign: 'center',
-    color: '#f2eddd', // Header text color
+    color: '#000000', // Header text color
   },
   section: {
     marginBottom: 20,
@@ -105,7 +107,7 @@ const styles = StyleSheet.create({
     width: 40,
     height: 40,
     borderRadius: 20,
-    backgroundColor: '#f2eddd',
+    backgroundColor: '#fefefe',
     justifyContent: 'center',
     alignItems: 'center',
   },
