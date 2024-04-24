194b3da9bc5a32c29e9c1a67afae5a5ae2ff69c4
krphruek <conner.krittaphong@gmail.com>
2024-04-24 17:57:17 +0700

    Update Service.jsx

--- src/Components/Service.jsx
+++ src/Components/Service.jsx
@@ -23,8 +23,6 @@ const Service = ({navigation}) => {
       </TouchableOpacity>
 
 
-
-
       <TouchableOpacity style={styles.chat} onPress={() => navigation.navigate('Clean')}>
       <View style={{flexDirection: 'row', alignItems: 'center'}}>
           <Image source={require('../Images/clean.jpg')} style={styles.chat_img}/>
